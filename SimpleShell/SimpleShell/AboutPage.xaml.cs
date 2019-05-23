8c2f6a5500a7f04458ced2f161f0203cc2d0101b
David Ortinau <david.ortinau@microsoft.com>
2019-05-22 20:01:04 -0500

    initial commit

---
+++ SimpleShell/SimpleShell/AboutPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace SimpleShell
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class AboutPage : ContentPage
+    {
+        public AboutPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            Device.OpenUri(new Uri("https://docs.microsoft.com/en-us/xamarin/xamarin-forms/app-fundamentals/shell/"));
+        }
+    }
+}
